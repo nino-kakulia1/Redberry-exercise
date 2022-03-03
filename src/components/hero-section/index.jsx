901006c930ed60ec13c27d26dfd2f5f20ce88162
nino-kakulia1 <78740756+nino-kakulia1@users.noreply.github.com>
2022-03-03 06:56:47 -0900

    feat(hero-section): added image

--- src/components/hero-section/index.jsx
+++ src/components/hero-section/index.jsx
@@ -1,5 +1,18 @@
+import Rocketman from "../../assets/rocketman.svg";
+
 function HeroSection() {
-  return <div className=""></div>;
+  return (
+    <div className="hero-section">
+      <h1>Welcome Rocketeer!</h1>
+      <a className="button-click" href="">
+        <button>Start Questionnaire</button>
+      </a>
+      <div className="hero-section__image">
+        <span>Submitted Applications</span>
+        <img src={Rocketman} alt="" />
+      </div>
+    </div>
+  );
 }
 
 export default HeroSection;
