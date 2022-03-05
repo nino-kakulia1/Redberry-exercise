31e89c8f61844dc204509c840f25064392052003
nino-kakulia1 <78740756+nino-kakulia1@users.noreply.github.com>
2022-03-05 05:23:19 -0900

    style(personal-info): created wrapeper and title

--- src/components/personal-info/index.jsx
+++ src/components/personal-info/index.jsx
@@ -6,6 +6,12 @@ function PersonalInfo() {
       <h1 className="personal-info__title">
         Hey, Rocketeer, what are your coordinates?
       </h1>
+      <div className="personal-info__wrapper">
+        <input type="text" placeholder="First Name" />
+        <input type="text" placeholder="Last Name" />
+        <input type="text" placeholder="E Mail" />
+        <input type="text" placeholder="+995 5__ __ __ __" />
+      </div>
     </div>
   );
 }
