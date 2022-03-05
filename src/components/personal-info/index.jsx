f99482bd94c2b7b7df983420c013b6ac36326a21
nino-kakulia1 <78740756+nino-kakulia1@users.noreply.github.com>
2022-03-05 05:33:18 -0900

    feat(personal-info): added input className

--- src/components/personal-info/index.jsx
+++ src/components/personal-info/index.jsx
@@ -7,10 +7,26 @@ function PersonalInfo() {
         Hey, Rocketeer, what are your coordinates?
       </h1>
       <div className="personal-info__wrapper">
-        <input type="text" placeholder="First Name" />
-        <input type="text" placeholder="Last Name" />
-        <input type="text" placeholder="E Mail" />
-        <input type="text" placeholder="+995 5__ __ __ __" />
+        <input
+          type="text"
+          className="personal-info__border"
+          placeholder="First Name"
+        />
+        <input
+          type="text"
+          className="personal-info__border"
+          placeholder="Last Name"
+        />
+        <input
+          type="text"
+          className="personal-info__border"
+          placeholder="E Mail"
+        />
+        <input
+          type="text"
+          className="personal-info__border"
+          placeholder="+995 5__ __ __ __"
+        />
       </div>
     </div>
   );
