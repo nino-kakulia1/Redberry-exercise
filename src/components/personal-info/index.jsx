18de083c0c630a524de68c4a965d5715f3f1b4ba
nino-kakulia1 <78740756+nino-kakulia1@users.noreply.github.com>
2022-03-06 01:44:39 -0900

    feat(persona-linfo): description

--- src/components/personal-info/index.jsx
+++ src/components/personal-info/index.jsx
@@ -1,4 +1,5 @@
 import Pagination from "../common/pagination";
+
 import "./style.scss";
 
 function PersonalInfo() {
@@ -31,6 +32,16 @@ function PersonalInfo() {
       </div>
 
       <Pagination />
+      <div className="about">
+        <h1 className="description__title">Redberry Origins</h1>
+        <p className="description__description">
+          You watch “What? Where? When?” Yeah. Our founders used to play it.
+          That’s where they got a question about a famous American author and
+          screenwriter Ray Bradbury. Albeit, our CEO Gaga Darsalia forgot the
+          exact name and he answered Ray Redberry. And at that moment, a name
+          for a yet to be born company was inspired - Redberry 😇
+        </p>
+      </div>
     </div>
   );
 }
