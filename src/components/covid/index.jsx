97451eee10f8835cc3eb7ec7a1eb04f9d8010fd1
nino-kakulia1 <78740756+nino-kakulia1@users.noreply.github.com>
2022-03-07 04:06:40 -0900

    fix(covid): question to wrapper

--- src/components/covid/index.jsx
+++ src/components/covid/index.jsx
@@ -4,8 +4,8 @@ function Covid() {
   return (
     <div className="covid">
       <h1 className="covid__title">Covid Stuff</h1>
-      <div className="covid__question">
-        <span>how would you prefer to work? </span>
+      <div className="covid__wrapper">
+        <span className="covid__question">how would you prefer to work? </span>
 
         <input type="radio" value=""></input>
         <label htmlFor=""> From Sairme Office</label>
