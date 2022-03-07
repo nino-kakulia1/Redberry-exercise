66127b17d6979b5581c089849f2e0fd6b3c2d7be
nino-kakulia1 <78740756+nino-kakulia1@users.noreply.github.com>
2022-03-07 03:25:37 -0900

    feat(covid); added covid section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/home-page";
 import PersonalInfo from "./components/personal-info";
 import Skills from "./components/skills";
+import Covid from "./components/covid";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/personal-info" element={<PersonalInfo />} />
           <Route path="/skills" element={<Skills />} />
+          <Route path="/skills" element={<Covid />} />
           <Route path="/" element={<Home />} />
         </Routes>
       </BrowserRouter>
