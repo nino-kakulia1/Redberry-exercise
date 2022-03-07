849265e1c1e4968fc7385e80bc310b0d172d20e8
nino-kakulia1 <78740756+nino-kakulia1@users.noreply.github.com>
2022-03-07 07:01:16 -0900

    feat(insight): added coment

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/home-page";
 import PersonalInfo from "./components/personal-info";
 import Skills from "./components/skills";
+import Insight from "./components/insight";
 import Covid from "./components/covid";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
           <Route path="/personal-info" element={<PersonalInfo />} />
           <Route path="/skills" element={<Skills />} />
           <Route path="/covid" element={<Covid />} />
+          <Route path="/covid" element={<Insight />} />
           <Route path="/" element={<Home />} />
         </Routes>
       </BrowserRouter>
---
+++ src/components/insight/index.jsx
@@ -0,0 +1,11 @@
+import "./style.scss";
+
+function Insight() {
+  return (
+    <div className="coment">
+      <h1>What about you?</h1>
+    </div>
+  );
+}
+
+export default Insight;
