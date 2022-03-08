ad5931a377824898ea0a257538322fa17b567cbb
nino-kakulia1 <78740756+nino-kakulia1@users.noreply.github.com>
2022-03-08 04:44:28 -0900

    fix(finish): index.jsx

---
+++ src/components/finish/index.jsx
@@ -0,0 +1,10 @@
+import "./style.scss";
+
+function Finish() {
+  return (
+    <div className="finish">
+      <h1 className="finish__title">Thanks for Joining 😊</h1>
+    </div>
+  );
+}
+export default Finish;
