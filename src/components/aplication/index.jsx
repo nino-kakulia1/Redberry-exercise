a03c65d4e5027f65f8ad201ee241b0e7054fcf39
nino-kakulia1 <78740756+nino-kakulia1@users.noreply.github.com>
2022-03-08 22:43:40 -0900

    feat(aplication): feat h1 element

--- src/components/aplication/index.jsx
+++ src/components/aplication/index.jsx
@@ -1,6 +1,11 @@
 import "./style.scss";
 
 function Aplication() {
-  return <div className="aplication"></div>;
+  return (
+    <div className="aplication">
+      <h1 className="aplication__title">Submitted Applications</h1>
+      .aplication
+    </div>
+  );
 }
 export default Aplication;
