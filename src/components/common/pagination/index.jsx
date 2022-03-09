deddc06ca100d495fc20f95258b2394a95e09253
nino-kakulia1 <78740756+nino-kakulia1@users.noreply.github.com>
2022-03-09 06:44:01 -0900

    feat(pagination): added steps navigation

--- src/components/common/pagination/index.jsx
+++ src/components/common/pagination/index.jsx
@@ -1,20 +1,33 @@
 import Left from "../../../assets/left.svg";
 import Right from "../../../assets/right.svg";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./style.scss";
 
+const steps = ["/personal-info", "/skills", "/covid", "/insight", "/submit"];
 function Pagination() {
+  const location = useLocation();
+
+  const locationIndex = steps.indexOf(location.pathname);
+
   return (
     <div className="pagination">
-      <img className="pagination__left-arrow" src={Left} alt="" />
+      <Link to={locationIndex === 0 ? "/" : steps[locationIndex - 1]}>
+        <img className="pagination__left-arrow" src={Left} alt="" />
+      </Link>
       <div className="pagination__container">
-        <div className="pagination__item"></div>
-        <div className="pagination__item"></div>
-        <div className="pagination__item"></div>
-        <div className="pagination__item"></div>
-        <div className="pagination__item"></div>
+        {steps.map((step, index) => (
+          <Link
+            className={`pagination__item ${
+              index <= locationIndex ? "pagination__item--active" : ""
+            }`}
+            key={step}
+            to={step}
+          />
+        ))}
       </div>
-      <img className="pagination__right-arrow" src={Right} alt="" />
+      <Link to={steps[locationIndex + 1]}>
+        <img className="pagination__right-arrow" src={Right} alt="" />
+      </Link>
     </div>
   );
 }
