388853d8379204e14311c9cc19c65a536155ffe7
nino-kakulia1 <78740756+nino-kakulia1@users.noreply.github.com>
2022-03-09 06:00:21 -0900

    fea(aplication): added input

--- src/components/aplication/index.jsx
+++ src/components/aplication/index.jsx
@@ -1,25 +1,76 @@
-import Vector from "../../assets/vector.svg";
-
 import "./style.scss";
 
 function Aplication() {
   return (
     <div className="aplication">
-      <h1 className="aplication__title">Submitted Applications</h1>
-      <div className="aplication__table">
-        <span className="aplication__number">1</span>
-        <img src={Vector} alt="" />
-      </div>
-      <div className="aplication__table">
-        <span className="aplication__number">2</span>
-        <img src={Vector} alt="" />
-      </div>
-      <div className="aplication__table">
-        <span className="aplication__number">3</span>
-        <img src={Vector} alt="" />
+      <div className="aplication__info">
+        <span>Personal Information</span>
+
+        <div className="information">
+          <label htmlFor=""> First Name</label>
+          <input type="" value="" placeholder="Eren"></input>
+        </div>
+        <div className="information">
+          <label htmlFor=""> Last Name</label>
+          <input type="" value="" placeholder="Yeager"></input>
+        </div>
+        <div className="information">
+          <label htmlFor="">E Mail</label>
+          <input
+            type=""
+            value=""
+            placeholder="eren@wings-of-freedom.ml"
+          ></input>
+        </div>
+        <div className="information">
+          <label htmlFor="">Phone</label>
+          <input type="" value="" placeholder="+995 591 93 50 80"></input>
+        </div>
+
+        <div className="situation">
+          <span>Covid Situation</span>
+          <div className="information">
+            <span>how would you prefer to work?</span>
+            <input type="radio" value="" placeholder=""></input>
+            <label htmlFor="">From Sairme Office</label>
+          </div>
+          <div className="information">
+            <span>Did you have covid 19?</span>
+            <input type="radio" value="" placeholder=""></input>
+            <label htmlFor="">Yes</label>
+            <label htmlFor="">No</label>
+          </div>
+          <div className="inforation__date">
+            <span>When did you have covid 19?</span>
+            <input type="" value="" placeholder="02/24/2022"></input>
+            <img src="" alt="" />
+          </div>
+        </div>
+        <div className="information">
+          <span>Have you been vaccinated?</span>
+          <input type="radio" value="" placeholder=""></input>
+          <label htmlFor="">Yes</label>
+          <input type="radio" value="" placeholder=""></input>
+          <label htmlFor="">No</label>
+        </div>
+        <div className="inforation__date">
+          <span>When did you get covid vaccine?</span>
+          <input type="" value="" placeholder="02/24/2022"></input>
+          <img src="" alt="" />
+        </div>
       </div>
 
-      <div className="personal-information"></div>
+      <div className="skillset">
+        <span>Skillset</span>
+        <div className="information">
+          <label htmlFor=""> PHP</label>
+          <input type="" value="" placeholder="Years of Experience: 3"></input>
+        </div>
+        <div className="information">
+          <label htmlFor="">React</label>
+          <input type="" value="" placeholder="Years of Experience: 9"></input>
+        </div>
+      </div>
     </div>
   );
 }
